8eb5f690ee1eada10e51d686db093d5697b9fef0
Diogo <diogofgsoares@gmail.com>
2024-03-02 17:13:06 +0000

    new pages
    add obras pages, for each service that was done, add logo, menu and change navbar to fit logo

--- app/_components/nav-bar.tsx
+++ app/_components/nav-bar.tsx
@@ -1,9 +1,38 @@
+import { Dropdown, MenuProps, Space } from "antd";
+
+const items: MenuProps["items"] = [
+  {
+    key: "1",
+    label: (
+      <a rel="noopener noreferrer" href="/obras/ladrilhos">
+        Ladrilhos
+      </a>
+    ),
+  },
+  {
+    key: "2",
+    label: (
+      <a rel="noopener noreferrer" href="/obras/canalizacoes">
+        Canalizações
+      </a>
+    ),
+  },
+  {
+    key: "3",
+    label: (
+      <a rel="noopener noreferrer" href="/obras/remodelacoes">
+        Remodelações
+      </a>
+    ),
+  },
+];
+
 const NavBar = () => {
   return (
-    <div className="w-full flex justify-center items-center h-32">
-      <ul className="flex flex-row gap-10 items-center">
+    <div className="w-full flex justify-center items-center h-48 bg-white nav-border">
+      <ul className="flex flex-row gap-10 items-center ">
         <li>
-          <img src="favicon.ico"></img>
+          <img className="w-52 h-40" src="/imgs/logo2.png"></img>
         </li>
         <li>
           <a href="/">Home</a>
@@ -12,7 +41,11 @@ const NavBar = () => {
           <a href="/about">Empresa</a>
         </li>
         <li>
-          <a href="/obras">Obras</a>
+          <Dropdown menu={{ items }}>
+            <a onClick={(e) => e.preventDefault()}>
+              <Space>Obras</Space>
+            </a>
+          </Dropdown>
         </li>
         <li>
           <a href="/services">Serviços</a>
---
+++ app/obras/ladrilhos/page.tsx
@@ -0,0 +1,127 @@
+"use client";
+import NavBar from "@/app/_components/nav-bar";
+import { Card, Col, Image, Row } from "antd";
+import Meta from "antd/es/card/Meta";
+
+export default function Ladrilhos() {
+  return (
+    <div className="bg-white h-screen">
+      <NavBar></NavBar>
+      <div className="bg-gray-100 section_container">
+        <div className="flex flex-col items-center">
+          <h1>Ladrilhos</h1>
+          <p>A nossa especialidade</p>
+        </div>
+        <div className="mt-20 w-full flex justify-center flex-col gap-10 items-center">
+          <Row className="flex justify-center" style={{ gap: "30px 70px" }}>
+            <Col span={6}>
+              <Card
+                hoverable
+                cover={
+                  <Image.PreviewGroup
+                    preview={{
+                      onChange: (current, prev) =>
+                        console.log(
+                          `current index: ${current}, prev index: ${prev}`
+                        ),
+                    }}
+                  >
+                    <Image
+                      className="w-full h-full object-cover"
+                      src="/imgs/ladrilhos/obra1/obra1-1.jpeg"
+                      style={{ maxHeight: "350px" }}
+                    />
+                    <Image
+                      style={{ display: "none" }}
+                      src="/imgs/ladrilhos/obra1/obra1-2.jpeg"
+                    />
+                    <Image
+                      className="w-full h-full object-cover"
+                      src="/imgs/ladrilhos/obra1/obra1-3.jpeg"
+                      style={{ display: "none" }}
+                    />
+                    <Image
+                      style={{ display: "none" }}
+                      src="/imgs/ladrilhos/obra1/obra1-4.jpeg"
+                    />
+                    <Image
+                      className="w-full h-full object-cover"
+                      src="/imgs/ladrilhos/obra1/obra1-5.jpeg"
+                      style={{ display: "none" }}
+                    />
+                    <Image
+                      style={{ display: "none" }}
+                      src="/imgs/ladrilhos/obra1/obra1-6.jpeg"
+                    />
+                    <Image
+                      className="w-full h-full object-cover"
+                      src="/imgs/ladrilhos/obra1/obra1-7.jpeg"
+                      style={{ display: "none" }}
+                    />
+                  </Image.PreviewGroup>
+                }
+              >
+                <Meta className="" title="Sintra" description="10/2023" />
+              </Card>
+            </Col>
+            <Col span={6}>
+              <Card
+                hoverable
+                cover={<img src="/imgs/carousel1.jpg" alt="Carousel5"></img>}
+              >
+                <Meta
+                  title="Europe Street beat"
+                  description="www.instagram.com"
+                />
+              </Card>
+            </Col>
+            <Col span={6}>
+              <Card
+                hoverable
+                cover={<img src="/imgs/carousel1.jpg" alt="Carousel5"></img>}
+              >
+                <Meta
+                  title="Europe Street beat"
+                  description="www.instagram.com"
+                />
+              </Card>
+            </Col>
+            <Col span={6}>
+              <Card
+                hoverable
+                cover={<img src="/imgs/carousel1.jpg" alt="Carousel5"></img>}
+              >
+                <Meta
+                  title="Europe Street beat"
+                  description="www.instagram.com"
+                />
+              </Card>
+            </Col>
+            <Col span={6}>
+              <Card
+                hoverable
+                cover={<img src="/imgs/carousel1.jpg" alt="Carousel5"></img>}
+              >
+                <Meta
+                  title="Europe Street beat"
+                  description="www.instagram.com"
+                />
+              </Card>
+            </Col>
+            <Col span={6}>
+              <Card
+                hoverable
+                cover={<img src="/imgs/carousel1.jpg" alt="Carousel5"></img>}
+              >
+                <Meta
+                  title="Europe Street beat"
+                  description="www.instagram.com"
+                />
+              </Card>
+            </Col>
+          </Row>
+        </div>
+      </div>
+    </div>
+  );
+}
